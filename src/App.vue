f28dc0005e6b547748c4e1175be18b22984f9c30
lianbinghua <lianbinghua@vipkid.com.cn>
2021-09-28 18:59:51 +0800

    vue-webpack-template

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="App">
+    <div>
+      <router-link to="/Home">go Home</router-link>
+      <router-link to="/About">go About</router-link>
+    </div>
+    <div>
+      <p>{{getCounter}}</p>
+      <button @click="add">add</button>
+    </div>
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  name: 'App',
+  data() {
+    return {}
+  },
+  created() {
+    this.testArr()
+  },
+  computed: {
+    ...mapGetters(['getCounter'])
+  },
+
+  methods: {
+    ...mapActions(['add']),
+    testArr() {
+      var arr = [1, 2, 3]
+      let b = Array.from(arr, x => x + x)
+      console.log(b)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './assets/styles/var.scss';
+.App {
+  text-align: center;
+  color: $primary-color;
+  font-size: 28px;
+}
+</style>
